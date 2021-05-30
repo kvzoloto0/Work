c19c27b887abfaf14eaca2ff1a4d9956503f7d5b
kvzoloto <kirill2000zol@gmail.com>
2021-05-30 18:01:39 +0300

    logn In

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { useTranslation } from 'react-i18next'
 import i18next from 'i18next'
 import cookies from 'js-cookie'
 import classNames from 'classnames'
-import signIn from './component/signIn/signIn'
 import './index.css'
 import UkraineIconsLanguage from './img/UkraineIconsLanguage.svg'
 import EnglishIconsLanguage from './img/EnglishIconsLanguage.svg'
+import SignIn from './component/SignIn/SignIn'
 
 const languages = [
   {
@@ -32,44 +32,52 @@ export default function App() {
 
 
   return (
+    <div>
   
-
-    <div className="container blockDimensionsCenter">
-      <div className="language-select blockDimensions">
-
+      {/* <div className="container blockDimensionsCenter">
+        <div className="language-select blockDimensions">
 
 
-      <div className="row">
-        <div className="col-12">
-          <h1 className="textDecorationSelectLanguage">{t('welcome_message')}</h1> 
-        </div>    
+
+        <div className="row">
+          <div className="col-12">
+            <h1 className="textDecorationSelectLanguage">{t('welcome_message')}</h1> 
+          </div>    
+        </div>
+
+
+
+        <div className="languageSelectionUkraineAndEnglish">
+          {languages.map(({ code, name, country_code }) => (        
+            <div key={country_code}>
+              <a
+                href="#"
+                className={classNames({
+                  disabled: currentLanguageCode === code,
+                })}
+                onClick={() => {
+                  i18next.changeLanguage(code)
+                }}
+              >
+                {name}
+              </a>                       
+            </div>
+          ))}
+        </div>
+
+        
+        </div>
+
+
+
+      </div> */}
+
+
+
+
+
+      <SignIn />
+
       </div>
-
-
-
-      <div className="languageSelectionUkraineAndEnglish">
-        {languages.map(({ code, name, country_code }) => (        
-          <div key={country_code}>
-            <a
-              href="#"
-              className={classNames({
-                disabled: currentLanguageCode === code,
-              })}
-              onClick={() => {
-                i18next.changeLanguage(code)
-              }}
-            >
-              {name}
-            </a>                       
-          </div>
-        ))}
-      </div>
-
-      
-      </div>
-
-
-
-    </div>
   )
 }
--- src/component/signIn/signIn.jsx
+++ src/component/signIn/signIn.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import './SignIn.css';
 
-const signIn = () => {
+
+const SignIn = () => {
     return (
         <div>
-
+            <div className="blockDimensionsCenter">
+                <div className="blockAppearanceLogiIn">
+                    <div className="blueSquare"></div>
+                    <div className="SupplierPortal">SUPPLIER PORTAL</div>
+                    <input className="emailLognIn" placeholder="Email"></input>
+                    <input className="passwordLognIn" placeholder="Password"></input>
+                    <div>
+                        <input className="radioLognIn" type="radio"></input>
+                        <span className="RememberUsername">Remember username</span>
+                    </div>
+                    <div>
+                        <button className="buttonSignUp">Sign up</button>
+                    </div>
+                    <div className="CreateAccount">Create account</div>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default signIn;
+export default SignIn;
